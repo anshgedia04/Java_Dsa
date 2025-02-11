3e10b332b5cf081d7fda5cac6931f887d31664f8
anshgedia04 <anshgedia03@gmail.com>
2025-02-11 17:54:41 +0530

    start

---
+++ javaBasics/one.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class one {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        System.out.println("num is : " + num1);
+    }
+}
---
+++ javaBasics/two.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class two {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("give num1 :");
+        int num1 = input.nextInt();
+        System.out.println("give num2 :");
+        int num2 = input.nextInt();
+        System.out.println("sum is : " + (num1 + num2));
+    }
+}

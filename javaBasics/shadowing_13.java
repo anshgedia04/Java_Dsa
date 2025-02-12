0de9535c21cbde3081a107c34655a7c512ba85a4
anshgedia04 <anshgedia03@gmail.com>
2025-02-12 22:11:16 +0530

    variable length of args

---
+++ javaBasics/shadowing_13.java
@@ -0,0 +1,14 @@
+public class shadowing_13 {
+    static int x = 210 ;
+    public static void main(String[] args) {
+        System.out.println(x);
+        int x = 20 ;
+        System.out.println(x);
+        func();
+        
+    }
+    static int func(){
+        System.out.println(x);
+        return x;
+    }
+}
---
+++ javaBasics/varArgs_14.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+//variable length arguments when we don't know the number of arguments which is passed to the function
+//we can  pass varArgs in last parameter of the function
+public class varArgs_14 {
+    public static void main(String[] args) {
+        func(1,2,3,4,5,6,7,8,9,10);
+        func2(1,2,"a","b","c");
+    }
+    static void func(int ...a){
+        System.out.println(Arrays.toString(a));
+    }
+
+    static void func2(int a ,int b , String ...c){
+        System.out.println(a + " " + b + " " + Arrays.toString(c));
+    }
+}

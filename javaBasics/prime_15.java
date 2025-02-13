0f6122e2e8f0ffeef7e22ca845a927ab78b30683
anshgedia04 <anshgedia03@gmail.com>
2025-02-13 15:25:36 +0530

    prime number

---
+++ javaBasics/prime_15.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class prime_15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        String ans = isPrime(n);
+        System.out.println(ans);
+    }
+
+    static String isPrime(int n){
+        if (n <= 1) {
+            return "not prime";
+        }
+        for(int i = 2 ; i <= n/2 ; i++){
+            if(n%i == 0){
+                return "not prime";
+            }
+            
+        }
+        return "prime" ;
+    }
+}

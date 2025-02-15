3f8e8f1319d5436c92f3146b1f5079e0329aa174
anshgedia04 <anshgedia03@gmail.com>
2025-02-15 13:33:25 +0530

    binary search

---
+++ javaBasics/BinarySearch_3/binarySearch_2.java
@@ -0,0 +1,27 @@
+package BinarySearch_3;
+
+public class binarySearch_2 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,7,8,9};
+        int res = Celing(arr , 6);
+        System.out.println(res);
+    }
+    static int Celing(int[] arr , int target){
+        int start = 0;
+        int end= arr.length - 1 ;
+
+        while(start <= end ){
+            int mid = (start + end) / 2 ;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if (target < arr[mid]) {
+                end=mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+}

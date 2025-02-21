35051602e0a47b7c1696adc82e6ba165b9d986b3
anshgedia04 <anshgedia03@gmail.com>
2025-02-21 19:02:46 +0530

    mountain array searched element

---
+++ LeetCode/binarySearch_5.java
@@ -0,0 +1,79 @@
+package LeetCode;
+//question : find element from an moutain array
+//example : int[] arr = {1,3,5,7,6,4,2,0};
+//output : 0
+//explanation : the element 0 is the smallest element in the array and it is present in the second half of the array
+//so we will search in the second half of the array
+//we will use binary search to find the element
+//we will first find the peak of the mountain array
+
+
+public class binarySearch_5 {
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,7,6,4,2,0};
+        int peak = peakMountain(arr);
+        int firstSearch = firstHalfElement(arr, 0, 0, peak);
+        if (firstSearch != -1) {
+            System.out.println(firstSearch);
+            return ;
+        }
+        int secondSearch = secondHalfElement(arr, 0, peak+1, arr.length-1);
+        if (secondSearch != -1) {
+            System.out.println(secondSearch);
+            return ;
+        }
+    }
+    //find the peak of the mountain array
+    static int peakMountain(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+        while (start <  end) {
+            int mid = (start+end) / 2 ;
+            if(arr[mid] > arr[mid+1]){
+                end = mid;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return start ;
+    }
+    //search in the first half of the array
+    static int firstHalfElement(int[] arr ,int target, int in1 , int in2){
+        int start = in1;
+        int end = in2;
+        int ans = -1 ;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            else if (target > arr[mid]) {
+                start = mid + 1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return ans;
+    }   
+    //search in the second half of the array
+    static int secondHalfElement(int[] arr ,int target, int in1 , int in2){
+        int start = in1;
+        int end = in2;
+        int ans = -1 ;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            else if (target > arr[mid]) {  
+                end = mid-1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+}

e9a0d3a9f0f10f489f8d532c3c8ccc58a0ed0638
anshgedia04 <anshgedia03@gmail.com>
2025-02-22 15:04:39 +0530

    rotated array

--- LeetCode/binarySearch_6.java
+++ LeetCode/binarySearch_6.java
@@ -4,8 +4,16 @@ public class binarySearch_6 {
         //rotated sorted array {5,6,7,0,1,2,3,4} =>
         int[] arr = {4,5,6,7,0,1,2,3};
 
-        // int res  = findPivot(arr);
+        int Rotated_peak_element  = findPivot(arr);
         // System.out.println(res);
+        int firstHalfElement = findTarget(arr, 2, 0, Rotated_peak_element);
+        if (firstHalfElement != -1) {
+            System.out.println(firstHalfElement);
+        }
+        else{
+            int res = findTarget(arr, 2, Rotated_peak_element, arr.length);
+            System.out.println(res);
+        }
     }
     static int findPivot(int[] arr){
         int start = 0;
@@ -28,4 +36,23 @@ public class binarySearch_6 {
         }
         return -1 ;
     }
+
+    static int findTarget(int[] arr ,int target, int in1 , int in2){
+        int start = in1;
+        int end = in2;
+        int ans = -1 ;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            else if (target > arr[mid]) {
+                start = mid + 1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return ans;
+    }   
 }

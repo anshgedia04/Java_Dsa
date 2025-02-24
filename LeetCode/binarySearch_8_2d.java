18660fbb2d6900fae4508eb1a6de6e6ff9dcd922
anshgedia04 <anshgedia03@gmail.com>
2025-02-24 19:04:17 +0530

    2d array search

--- LeetCode/binarySearch_8_2d.java
+++ LeetCode/binarySearch_8_2d.java
@@ -1,7 +1,7 @@
 package LeetCode;
 
 import java.util.Arrays;
-
+// search target in 2d array by linear search
 public class binarySearch_8_2d {
     public static void main(String[] args) {
         int[][] arr = {{1,2,3},

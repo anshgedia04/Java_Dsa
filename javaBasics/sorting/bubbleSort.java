e857aa9410c8fb02183ca9ce78bc88450cdf37fd
anshgedia04 <anshgedia03@gmail.com>
2025-02-26 12:09:19 +0530

    sort

--- javaBasics/sorting/bubbleSort.java
+++ javaBasics/sorting/bubbleSort.java
@@ -15,8 +15,8 @@ public class bubbleSort {
     // }
     static void bubbleSort(int[] arr){
         
-        for(int i = 0 ; i < arr.length ; i++){
-            for(int j= 1  ; j <= arr.length - i - 1 ; j++ ){
+        for(int i = 0 ; i < arr.length; i++){
+            for(int j= 1  ; j < arr.length - i  ; j++ ){
                 if (arr[j - 1] > arr[j]) {
                     int temp = arr[j];
                     arr[j] = arr[j-1];

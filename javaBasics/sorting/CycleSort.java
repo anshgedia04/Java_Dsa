532e676cf008f29623beeff324e9e5da1ccf77f3
anshgedia04 <anshgedia03@gmail.com>
2025-02-26 14:24:25 +0530

    cycle  Sort

---
+++ javaBasics/sorting/CycleSort.java
@@ -0,0 +1,33 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class CycleSort {
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 2, 1, 4};
+        cycleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void cycleSort(int[] arr) {
+        for (int i = 0; i < arr.length; ) { // No i++ here, we only increment when the element is correctly placed
+            int correctIndex = arr[i] - 1; // The correct position for arr[i]
+
+            if (arr[i] == arr[correctIndex]) { 
+                i++; // Move to the next index only when arr[i] is correctly placed
+
+                // if (i+1 == arr[i]) { 
+                //     i++; // Move to the next index only when arr[i] is correctly placed
+            } else {
+                swap(arr, i, correctIndex);
+
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}

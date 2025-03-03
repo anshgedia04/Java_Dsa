a234792825e2c7ab12a4d22fd1c9d222a5a15efb
anshgedia04 <anshgedia03@gmail.com>
2025-03-03 17:34:02 +0530

    pattern

--- javaBasics/Pattern/Pattern4.java
+++ javaBasics/Pattern/Pattern4.java
@@ -4,7 +4,7 @@ public class Pattern4 {
     public static void main(String[] args) {
         int N = 4;
         for (int row = 1; row <= 2 * N - 1; row++) {
-            int totalCols = row > N ? 2 * N - row : row;
+            int totalCols = row > N ?  N - (row - N): row;
             for (int col = 1; col <= totalCols; col++) { 
                 System.out.print("* "); 
             }

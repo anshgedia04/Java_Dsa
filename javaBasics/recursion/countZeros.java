90db4e7493e78235d7ee40e3d080905172a918c9
anshgedia04 <anshgedia03@gmail.com>
2025-03-08 12:48:52 +0530

    count number of zeros

---
+++ javaBasics/recursion/countZeros.java
@@ -0,0 +1,25 @@
+package recursion;
+
+public class countZeros {
+    public static void main(String[] args) {
+        int n = 3211 ;
+        int ans = count(n);
+        System.out.println(ans);
+    }
+    static int count(int n){
+        return note(n , 0);
+    }
+
+    static int note(int n , int c){
+        if (n==0) {
+            return c;
+        }
+        int rem = n%10;
+        if (rem == 0) {
+          return  note(n/10, c+1);
+        }
+        else{
+            return note(n/10 , c);
+        }
+    }
+}

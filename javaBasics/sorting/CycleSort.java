5e073425878415f3c7e4e5209e80236746b18b2e
anshgedia04 <anshgedia03@gmail.com>
2025-04-06 11:27:22 +0530

    inc

--- javaBasics/sorting/CycleSort.java
+++ javaBasics/sorting/CycleSort.java
@@ -10,7 +10,7 @@ public class CycleSort {
     }
 
     static void cycleSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) { // No i++ here, we only increment when the element is correctly placed
+        for (int i = 0; i < arr.length; ) { // No i++ here, we only increment when the element is correctly placed
             int correctIndex = arr[i] - 1; // The correct position for arr[i]
 
             if (arr[i] == arr[correctIndex]) { 

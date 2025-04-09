1af211562d3ce3033a5d191bfb705adf4b70a3a5
anshgedia04 <anshgedia03@gmail.com>
2025-04-09 22:49:28 +0530

    fibbo series

---
+++ javaBasics/recursion/Fibbo_series.java
@@ -0,0 +1,19 @@
+package recursion;
+
+public class Fibbo_series {
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n; // Base case: F(0) = 0, F(1) = 1
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2); // Recursive case
+    }
+
+    public static void main(String[] args) {
+        int n = 5; // Number of terms to print
+
+        System.out.print("Fibonacci series up to " + n + " terms: ");
+        for (int i = 0; i <= n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+}

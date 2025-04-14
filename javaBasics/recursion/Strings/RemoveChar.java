5ead580f099fa9fc694e00a2bdd079269dc77c6f
anshgedia04 <anshgedia03@gmail.com>
2025-04-14 15:44:33 +0530

    remove char using recr

---
+++ javaBasics/recursion/Strings/RemoveChar.java
@@ -0,0 +1,21 @@
+package recursion.Strings;
+
+public class RemoveChar {
+    public static void main(String[] args) {
+        removeChar("", "ansh");
+    }
+
+    static void removeChar(String updated_str , String main_str){
+        if (main_str.length() == 0) {
+            System.out.println(updated_str);
+            return; 
+        }
+        char ch = main_str.charAt(0);
+        if (ch == 'a') {
+            removeChar(updated_str, main_str.substring(1));
+        }
+        else{
+            removeChar(updated_str + ch , main_str.substring(1));
+        }
+    }
+}

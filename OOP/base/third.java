92cb12129d504c5d0e23c3cc9822c76d5ea925ed
anshgedia04 <anshgedia03@gmail.com>
2025-04-18 18:11:28 +0530

    oop first

--- OOP/base/third.java
+++
@@ -1,5 +0,0 @@
-package OOP.base;
-
-public class third {
-    
-}
---
+++ oop/one.java
@@ -0,0 +1,15 @@
+
+//class is a templete for objects 
+//objects are instance of a class
+class Student{
+    int age;
+    String name;
+
+}
+public class one {
+   public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "nsh";
+        System.out.println(s1.name);
+   } 
+}

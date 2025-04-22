c981eb7cd58642aa183eb2ad235df188cc2fba9e
anshgedia04 <anshgedia03@gmail.com>
2025-04-22 13:37:56 +0530

    oop static

--- oop/static_folder/static_learn.java
+++ oop/static_folder/static_learn.java
@@ -3,7 +3,7 @@ package oop.static_folder;
 //by usimg static we can access variables and other stuff without creating any object it is class dependent not on object
 
 //why we write always public static void main(String[] args)
-// ans : bcz we know that main is the first function that will execute. if we not put stativ over that so that would require
+// ans : bcz we know that main is the first function that will execute. if we not put static over that so that would require
             //object which we cant do .
 
 //we can not use this keyword inside static function bcz this is reference to current object and static is not object depedent

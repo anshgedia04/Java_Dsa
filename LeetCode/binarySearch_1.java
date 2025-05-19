f44adc3857b3df20d7e427d1d6e2b6b468153604
anshgedia04 <anshgedia03@gmail.com>
2025-05-19 20:26:48 +0530

    binary

--- LeetCode/binarySearch_1.java
+++ LeetCode/binarySearch_1.java
@@ -1,6 +1,6 @@
 package LeetCode;
 //find the next letter of the target letter in the array
-//example: arr={'a' , 'b' , 'c' , 'e'} and target='b' then next letter is 'c'
+//example: arr={'a' , 'b' , 'c' , 'e'} and target='b' then next letter is
 public class binarySearch_1 {
     public static void main(String[] args) {
         char[] arr =  {'a' , 'b' , 'c' , 'e'};
